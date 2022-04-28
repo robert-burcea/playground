e78a011a01d8ab0c689211534bbc693b9841904b
rburcea <robert.burcea@gmail.com>
2022-04-28 19:26:45 +0300

    create separate id's

--- src/Calendar.js
+++ src/Calendar.js
@@ -4,13 +4,15 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import interaction from '@fullcalendar/interaction'
 import {events} from './Events'
 import {someObject} from './Events'
+import {v4 as uuidv4} from 'uuid';
 //IMPORTING THE SMALL TOOL TIPPY.js THAT CREATES THE TOOLTIP THAT HOVERS
 import tippy from 'tippy.js'
+import 'tippy.js/themes/material.css';
 
 export default function Calendar() {
     //THE FOLLOWING MAP ADDS TO THE EVENT OBJECT YOUR NEW OBJECT
     events.map((e) => {
-        e.extendedProps = someObject; // THE someObject is imported from Events.js, is your future object
+        e.extendedProps = {...someObject, id:uuidv4()}; // THE someObject is imported from Events.js, is your future object
         //e.className = JSON.stringify(e.extendedProps.id); NOT NEEDED ANYMORE
         return e;
     })    
@@ -18,9 +20,11 @@ export default function Calendar() {
       //THIS CREATES THE TOOLTIP THAT WILL APPEAR UPON HOVERING, CREATED AT THE LOCATION OF THE EVENT
       //WHICH IS info.el(html element)
       tippy(info.el, {
-        content:`THE ID: ${info.event.extendedProps.id}, THE NAME:${info.event.extendedProps.name},
-        THE OCCUPATION: ${info.event.extendedProps.occupation}`,
-        theme: 'solid'
+        content: `<strong>NAME</strong>:${info.event.extendedProps.name}<br>
+        <strong>ID</strong>:${info.event.extendedProps.id}<br>
+        <strong>OCCUPATION</strong>:${info.event.extendedProps.occupation}`,
+        theme: 'material',
+        allowHTML: true
       })
     }
     console.log(events)
